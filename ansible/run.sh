de21081948c5966165916c569caa052befd3b031
Steven Tang <steven.yc.tang@gmail.com>
2020-04-11 19:53:47 +1000

    ansible: install docker/compose

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Path to private key
+KEY_FILE=~/.ssh/steven
+# Key name for VM
+KEY_NAME=Steven
+
 chmod +x unimelb-comp90024-2020-grp-42-openrc.sh
 . ./unimelb-comp90024-2020-grp-42-openrc.sh
 
-ansible-playbook --ask-become-pass -u ubuntu --key-file=~/.ssh/steven openstack-mrc.yaml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
+    -u ubuntu --key-file=$KEY_FILE \
+    -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
+    openstack-mrc.yaml
