242a3b45f3ea5bf923dfbc07b8a128d55c132d31
Steven Tang <steven.yc.tang@gmail.com>
2020-04-11 17:49:28 +1000

    ansible: restructure and install remote deps

--- ansible/run.sh
+++ ansible/run.sh
@@ -3,4 +3,4 @@
 chmod +x unimelb-comp90024-2020-grp-42-openrc.sh
 . ./unimelb-comp90024-2020-grp-42-openrc.sh
 
-ansible-playbook --ask-become-pass openstack-mrc.yaml
+ansible-playbook --ask-become-pass -u ubuntu --key-file=~/.ssh/steven openstack-mrc.yaml
