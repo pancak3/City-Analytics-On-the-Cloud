4c1deea8ac2d7e509911988a1eacba7e0e62bbf6
Steven Tang <steven.yc.tang@gmail.com>
2020-04-11 21:55:08 +1000

    ansible: separate

--- ansible/run.sh
+++ ansible/run.sh
@@ -8,7 +8,16 @@ KEY_NAME=Steven
 chmod +x unimelb-comp90024-2020-grp-42-openrc.sh
 . ./unimelb-comp90024-2020-grp-42-openrc.sh
 
+# Run from start
+# ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
+#     -u ubuntu --key-file=$KEY_FILE \
+#     -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
+#     --tags all \
+#     openstack-mrc.yaml
+
+# Only run configure playbook
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
     -u ubuntu --key-file=$KEY_FILE \
     -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
+    --tags instance_info --tags configure \
     openstack-mrc.yaml
