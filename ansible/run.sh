bfae79d0d86488268cb713c78ffd9b02edd40ad5
Steven Tang <steven.yc.tang@gmail.com>
2020-04-21 17:39:45 +1000

    doc: format and change script

--- ansible/run.sh
+++ ansible/run.sh
@@ -11,8 +11,8 @@ KEY_NAME=$(head -n 1 ssh_key.config)
 init() {
     runable
     echo "[*] Initialising ... "
-    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
-        -u ubuntu --key-file=$KEY_FILE \
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+        -u ubuntu --key-file="$KEY_FILE" \
         -e "ansible_python_interpreter=python3 key_name=$KEY_NAME" \
         --tags all \
         main.yaml
@@ -22,8 +22,8 @@ init() {
 configure() {
     runable
     echo "[*] Configuring ... "
-    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
-        -u ubuntu --key-file=$KEY_FILE \
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+        -u ubuntu --key-file="$KEY_FILE" \
         -e "ansible_python_interpreter=python3 key_name=$KEY_NAME" \
         --tags instance_info --tags configure \
         main.yaml
@@ -32,7 +32,7 @@ configure() {
 arg() {
     if [[ "$1" = "-i" ]]; then
         init
-    elif [[ $1 = "-c" ]]; then
+    elif [[ "$1" = "-c" ]]; then
         configure
     fi
 }
@@ -42,19 +42,13 @@ runable() {
     . ./unimelb-comp90024-2020-grp-42-openrc.sh
 }
 
-usage() { 
-	echo -e "Initialise cluster:\n\t $0 -i\n" 
-	echo -e "Configure cluster:\n\t $0 -c\n" 
-	echo -e "Or both:\n\t $0 -i -c" 
-	} 
+usage() {
+    echo -e "Initialise and configure cluster:\n\t $0 -i\n"
+    echo -e "Configure cluster:\n\t $0 -c\n"
+}
 
-if [[ ! -z $1 ]]; then
-    if [[ ! -z $2 ]]; then
-        arg $1
-        arg $2
-    else
-        arg $1
-    fi
+if [[ -n "$1" ]]; then
+    arg "$1"
 else
     usage
 fi
