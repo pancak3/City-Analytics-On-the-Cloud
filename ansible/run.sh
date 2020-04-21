7547dd161d479bc37e56993146daf0215db6741a
Qifan <plocircle@live.com>
2020-04-21 16:43:12 +0800

    README (#1)
    * 1. Adjust the host environment configuration.
    2. Add readme.
    3. Use docker to create couch directory.
    
    * bug: Cluster setup timed out waiting for nodes to connect
    
    * doc: format and change script
    
    * fix: only run finish setup when cluster not set up
    
    * fix script: get the right KEY_NAME
    
    * fix: cluster reconnection
    
    Co-authored-by: Steven Tang <steven.yc.tang@gmail.com>

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,23 +1,54 @@
 #!/bin/bash
 
+# Exit when any err occurs
+set -o errexit
 # Path to private key
-KEY_FILE=~/.ssh/steven
+KEY_FILE=$(head -n 1 ssh_key.config)
 # Key name for VM
-KEY_NAME=Steven
+KEY_NAME=$(tail -n 1 ssh_key.config)
 
-chmod +x unimelb-comp90024-2020-grp-42-openrc.sh
-. ./unimelb-comp90024-2020-grp-42-openrc.sh
+# Initialise cluster
+init() {
+    runable
+    echo "[*] Initialising ... "
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+        -u ubuntu --key-file="$KEY_FILE" \
+        -e "ansible_python_interpreter=python3 key_name=$KEY_NAME" \
+        --tags all \
+        main.yaml
+}
 
-# Run from start
-# ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
-#     -u ubuntu --key-file=$KEY_FILE \
-#     -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
-#     --tags all \
-#     main.yaml
+# Configure
+configure() {
+    runable
+    echo "[*] Configuring ... "
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+        -u ubuntu --key-file="$KEY_FILE" \
+        -e "ansible_python_interpreter=python3 key_name=$KEY_NAME" \
+        --tags instance_info --tags configure \
+        main.yaml
+}
 
-# Only run configure playbook
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
-    -u ubuntu --key-file=$KEY_FILE \
-    -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
-    --tags instance_info --tags configure \
-    main.yaml
+arg() {
+    if [[ "$1" = "-i" ]]; then
+        init
+    elif [[ "$1" = "-c" ]]; then
+        configure
+    fi
+}
+
+runable() {
+    chmod +x unimelb-comp90024-2020-grp-42-openrc.sh
+    . ./unimelb-comp90024-2020-grp-42-openrc.sh
+}
+
+usage() {
+    echo -e "Initialise and configure cluster:\n\t $0 -i\n"
+    echo -e "Configure cluster:\n\t $0 -c\n"
+}
+
+if [[ -n "$1" ]]; then
+    arg "$1"
+else
+    usage
+fi
