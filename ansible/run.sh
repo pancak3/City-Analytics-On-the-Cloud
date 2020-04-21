5571d01d5988020c86bd1a669f3f5bb9ada8df96
Qifan <plocircle@live.com>
2020-04-21 13:24:15 +0800

    1. Adjust the host environment configuration. 2. Add readme. 3. Use docker to create couch directory.

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
+# Exit when any err occurs
+set -o errexit
+set -o nounset
 # Path to private key
-KEY_FILE=~/.ssh/steven
+KEY_FILE=$(head -n 1 ssh_key.config)
 # Key name for VM
-KEY_NAME=Steven
+KEY_NAME=$(tail -n 1 ssh_key.config)
 
 chmod +x unimelb-comp90024-2020-grp-42-openrc.sh
 . ./unimelb-comp90024-2020-grp-42-openrc.sh
 
-# Run from start
-# ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
-#     -u ubuntu --key-file=$KEY_FILE \
-#     -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
-#     --tags all \
-#     main.yaml
-
 # Only run configure playbook
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --ask-become-pass \
     -u ubuntu --key-file=$KEY_FILE \
-    -e "ansible_python_interpreter=/usr/bin/python3 key_name=$KEY_NAME" \
+    -e "ansible_python_interpreter=python3 key_name=$KEY_NAME" \
     --tags instance_info --tags configure \
     main.yaml
