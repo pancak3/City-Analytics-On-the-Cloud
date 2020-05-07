b92bb6aad86befaa9a338ef5ccf14c07a085509b
Steven Tang <steven.yc.tang@gmail.com>
2020-05-07 22:12:08 +1000

    ansible: hopefully a db network

--- app/api/app.js
+++ app/api/app.js
@@ -1,8 +1,29 @@
+const path = requrie('path');
 const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
 const app = express();
 
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+// For local development, bad practice for production use
+app.use(cors());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Root
 app.get('/', (req, res) => {
     return res.send('COMP90024');
+});
+
+// Database status
+app.get('/api', proxy(`http://${username}:${password}@couchdb`));
+
+// Frontend
+app.get('*', function(req, res) {
+    return res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
