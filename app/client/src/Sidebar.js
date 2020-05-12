15a9b3b088f84ef1ea1b942bacded77faa9796cc
Steven Tang <steven.yc.tang@gmail.com>
2020-05-12 19:11:26 +1000

    app: better styling

--- app/client/src/Sidebar.js
+++ app/client/src/Sidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList } from '@fortawesome/free-solid-svg-icons';
 
 class Sidebar extends React.Component {
     render() {
@@ -8,24 +10,27 @@ class Sidebar extends React.Component {
                 <h1>COMP90024 Assignment 2</h1>
                 <ul>
                     <li>
-                        <NavLink to="/">Summary</NavLink>
+                        <NavLink exact activeClassName="selected" to="/">
+                            <FontAwesomeIcon icon={faList} />Summary
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/s1">Scenario 1</NavLink>
+                        <NavLink activeClassName="selected" to="/s1">Scenario 1</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/s2">Scenario 2</NavLink>
+                        <NavLink activeClassName="selected" to="/s2">Scenario 2</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/s3">Scenario 3</NavLink>
+                        <NavLink activeClassName="selected" to="/s3">Scenario 3</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/s4">Scenario 4</NavLink>
+                        <NavLink activeClassName="selected" to="/s4">Scenario 4</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/s5">Scenario 5</NavLink>
+                        <NavLink activeClassName="selected" to="/s5">Scenario 5</NavLink>
                     </li>
                 </ul>
+                <p>Made by: Team 42</p>
             </aside>
         );
     }
