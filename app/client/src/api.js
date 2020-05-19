dd8d15197ccd97cf15abbbc93a47b0fc326e5f12
Steven Tang <steven.yc.tang@gmail.com>
2020-05-19 17:33:18 +1000

    app: fix url again

--- app/client/src/api.js
+++ app/client/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = process.env.URL || 'http://localhost:3000';
+const BASE_URL = process.env.URL === undefined ? 'http://localhost:3000': '';
 
 export function getStats() {
     return axios.get(`${BASE_URL}/api/stats`).then((response) => {
