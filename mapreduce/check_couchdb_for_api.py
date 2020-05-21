fa094e474392b88a7d2a4384d7e8b468764bdd5f
Qifan <plocircle@live.com>
2020-05-21 11:14:33 +0800

    mapreduce: finished mapreduce checking scripts and backup

--- mapreduce/check_couchdb_for_api.py
+++ mapreduce/check_couchdb_for_api.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+from time import asctime, localtime, time
 from tqdm import tqdm
 from utils.database import CouchDB
 from utils.logger import get_logger
@@ -9,6 +10,39 @@ from cloudant.design_document import DesignDocument, Document
 logger = get_logger('AreasUpdater', logging.DEBUG)
 
 couch = CouchDB()
+date_time = asctime(localtime(time()))
+
+backup_path = os.path.join(os.path.join("views_backup", date_time))
+
+count = 1
+while os.path.exists(backup_path):
+    backup_path = backup_path[:backup_path.find('-')]
+    backup_path += str(count)
+    count += 1
+    if count > 10:
+        logger.error("Cannot generate a valid  backup path")
+        exit()
+
+
+class View:
+    def __init__(self, map_, reduce):
+        self.map = map_
+        self.reduce = reduce
+
+
+def read_view_from_file(path):
+    map_file = open(os.path.join(path, 'map.js'))
+    map_func = map_file.read()
+    map_file.close()
+    map_func = map_func[12:]
+
+    reduce_file = open(os.path.join(path, 'reduce.js'))
+    reduce_func = reduce_file.read()
+    reduce_file.close()
+    reduce_func = reduce_func[15:]
+    if reduce_func[0] == '_':
+        reduce_func = reduce_func[:reduce_func.find(';')]
+    return View(map_func, reduce_func)
 
 
 def update_areas():
@@ -47,52 +81,89 @@ def update_areas():
 
 
 def check_db(combined_db_name):
+    os.mkdir(os.path.join("views_backup", date_time, combined_db_name))
     (db_name, partitioned) = combined_db_name.split('.')
     partitioned = True if partitioned == "partitioned" else False
     if db_name not in couch.client.all_dbs():
         couch.client.create_database(db_name, partitioned=partitioned)
         logger.info("Created database: {}".format(db_name))
 
+    for ddoc_name in os.listdir(os.path.join("couch", combined_db_name)):
+        if os.path.isdir(os.path.join("couch", combined_db_name, ddoc_name)):
+            check_ddoc_in_db(combined_db_name, ddoc_name)
 
-def ddoc_exists(db_name, ddoc_name):
-    db_name = db_name.split('.')[0]
-    (ddoc_name, partitioned) = ddoc_name.split('.')
+
+def check_ddoc_in_db(combined_db_name, combined_ddoc_name):
+    os.mkdir(os.path.join("views_backup", date_time, combined_db_name, combined_ddoc_name))
+    db_name = combined_db_name.split('.')[0]
+    (ddoc_name, partitioned) = combined_ddoc_name.split('.')
     partitioned = True if partitioned == "partitioned" else False
     design_doc = Document(couch.client[db_name], '_design/' + ddoc_name)
     if not design_doc.exists():
         design_doc = DesignDocument(couch.client[db_name], '_design/' + ddoc_name, partitioned=partitioned)
         design_doc.save()
         logger.info("Created design document: {}".format(ddoc_name))
-        return False
-    return True
+
+    for view_name in os.listdir(os.path.join("couch", combined_db_name, combined_ddoc_name)):
+        if os.path.isdir(os.path.join("couch", combined_db_name, combined_ddoc_name, view_name)):
+            check_a_single_view(combined_db_name, combined_ddoc_name, view_name)
 
 
-def update_view(db_name, ddoc_name, view_name):
-    db_name = db_name.split('.')[0]
-    (ddoc_name, partitioned) = ddoc_name.split('.')
+def update_view(db_name, ddoc_name, view_name, local_view, partitioned):
     partitioned = True if partitioned == "partitioned" else False
-    design_doc = DesignDocument(couch.client[db_name], '_design/' + ddoc_name, partitioned=partitioned)
+    design_doc = couch.client[db_name]['_design/' + ddoc_name]
+    design_doc.add_view(view_name, local_view.map, local_view.reduce, partitioned=partitioned)
+    design_doc.save()
+    logger.info("Updated view: {}/_design/{}/_view/{}".format(db_name, ddoc_name, view_name))
 
 
-def check_a_single_view():
-    pass
+def save_js(path, content):
+    f = open(path, "w+")
+    f.write(content)
+    f.close()
 
 
-def check_views():
-    # TODO check views
+def check_a_single_view(combined_db_name, combined_ddoc_name, view_name):
+    os.mkdir(os.path.join("views_backup", date_time, combined_db_name, combined_ddoc_name, view_name))
+
+    db_name = combined_db_name.split('.')[0]
+    (ddoc_name, partitioned) = combined_ddoc_name.split('.')
+    partitioned = True if partitioned == "partitioned" else False
+    local_view = read_view_from_file(os.path.join("couch", combined_db_name, combined_ddoc_name, view_name))
+    design_doc = couch.client[db_name]['_design/' + ddoc_name]
+
+    # backup
+    map_func = "const map = " + design_doc.views[view_name]['map']
+    if design_doc.views[view_name]['reduce'][0] == '_':
+        reduce_func = "const reduce = " + design_doc.views[view_name]['reduce'] + ';\n'
+    else:
+        reduce_func = "const reduce = " + design_doc.views[view_name]['reduce']
+    save_js(os.path.join("views_backup",
+                         date_time, combined_db_name, combined_ddoc_name, view_name, 'map.js'), map_func)
+    save_js(os.path.join("views_backup",
+                         date_time, combined_db_name, combined_ddoc_name, view_name, 'reduce.js'), reduce_func)
+    if view_name in design_doc.views:
+        if design_doc.views[view_name]['map'] != local_view.map \
+                or design_doc.views[view_name]['reduce'] != local_view.reduce:
+            design_doc.delete_view(view_name)
+            design_doc.save()
+            update_view(db_name, ddoc_name, view_name, local_view, partitioned)
+    else:
+        update_view(db_name, ddoc_name, view_name, local_view, partitioned)
+
+
+def check_all_dbs():
     for db_name in os.listdir("couch"):
         if os.path.isdir(os.path.join("couch", db_name)):
             check_db(db_name)
-            for ddoc_name in os.listdir(os.path.join("couch", db_name)):
-                if os.path.isdir(os.path.join("couch", db_name, ddoc_name)):
-                    if ddoc_exists(db_name, ddoc_name):
-                        for view_name in os.listdir(os.path.join("couch", db_name, ddoc_name)):
-                            if os.path.isdir(os.path.join("couch", db_name, ddoc_name, view_name)):
-                                check_a_single_view()
+    logger.info("Views full backup is under {}".format(backup_path))
 
 
 if __name__ == '__main__':
-    if not os.path.exists("views_backup"):
-        os.mkdir("views_backup")
-    # update_areas()
-    check_views()
+    try:
+        os.mkdir('views_backup')
+        os.mkdir(backup_path)
+        update_areas()
+        check_all_dbs()
+    except Exception as e:
+        logger.error(e)
