0964cf3e8ab70d0e2c451f5b710df93a2ced1d0e
Qifan <plocircle@live.com>
2020-05-24 01:00:36 +0800

    WIP: analysis: include aurin data

--- analysis/utils/getAurindata.py
+++ analysis/utils/getAurindata.py
@@ -1,26 +1,31 @@
 import json
 import couchdb
 import logging
+from utils.database import CouchDB
 from utils.logger import get_logger
 
 logger = get_logger('Models', logging.DEBUG)
 
+couch_db = CouchDB()
+ier_file = 'AURIN_data/Economic_resource.geojson'
+ieo_file = 'AURIN_data/Education_Occupation.geojson'
+db_names = ['aurin_ier', 'aurin_ieo']
+for db_name in db_names:
+    if db_name not in couch_db.client.all_dbs():
+        couch_db.client.create_database(db_name)
 
-couch = couchdb.Server('https://comp90024:pojeinaiShoh9Oo@comp90024.steven.cf')
-ierfile = 'AURIN_data/Economic_resource.geojson'
-ieofile = 'AURIN_data/Education_Occupation.geojson'
-db_ier = couch.create('aurindata_ier')
-db_ieo = couch.create('aurindata_ieo')
 
-
-def loadAURINdata_ier(jsonfile):
-    '''
+def load_aurin_data_ier(jsonfile):
+    """
     The Index of Economic Resources (IER) focuses on the financial aspects of relative
     socio-economic advantage and disadvantage, by summarising variables related to income
     and wealth. This index excludes education and occupation variables because they are
     not direct measures of economic resources.
-    '''
-    with open(jsonfile, 'r', errors = 'ignore') as f:
+    :param jsonfile:
+    :return:
+    """
+
+    with open(jsonfile, 'r', errors='ignore') as f:
         data = json.load(f)
         for key in data['features']:
             property = key['properties']
@@ -28,16 +33,15 @@ def loadAURINdata_ier(jsonfile):
                 SA2_id = property['sa2_main16']
                 pop = property['usual_res_pop']
                 ier_score = property['ier_score']
-                doc = {'SA2_id': SA2_id, 'population':pop, 'ier_score':ier_score}
+                doc = {'SA2_id': SA2_id, 'population': pop, 'ier_score': ier_score}
                 db_ier.save(doc)
 
     logger.debug("Saved {} results".format(len(doc)))
 
 
-
 def loadAURINdata_ieo(jsonfile):
-    #load IEO data from AURIN dataset
-    with open(jsonfile, 'r', errors = 'ignore') as f:
+    # load IEO data from AURIN dataset
+    with open(jsonfile, 'r', errors='ignore') as f:
         data = json.load(f)
         for key in data['features']:
             property = key['properties']
@@ -45,10 +49,11 @@ def loadAURINdata_ieo(jsonfile):
                 SA2_id = property['sa2_main16']
                 pop = property['usual_res_pop']
                 ieo_score = property['ieo_score']
-                doc = {'SA2_id': SA2_id, 'population':pop, 'ieo_score':ieo_score}
+                doc = {'SA2_id': SA2_id, 'population': pop, 'ieo_score': ieo_score}
                 db_ieo.save(doc)
 
     logger.debug("Saved {} results".format(len(doc)))
 
-loadAURINdata_ier(ierfile)
-loadAURINdata_ier(ieofile)
+
+load_aurin_data_ier(ier_file)
+load_aurin_data_ier(ieo_file)
