203838b49f1ce63e1bdb86d406530b3c02cf2644
Qifan <plocircle@live.com>
2020-05-24 10:45:31 +0800

    harvest: add tools to dump database to json file

---
+++ harvest/tools/dump_data/write.py
@@ -0,0 +1,24 @@
+import json
+
+from tqdm import tqdm
+from cloudant.client import Cloudant
+
+# target_couch = Cloudant('comp90024', 'pojeinaiShoh9Oo', url='http://127.0.0.1:5984', connect=True)
+target_couch = Cloudant('admin', 'password', url='http://127.0.0.1:5984/', connect=True)
+target_couch.connect()
+client = target_couch['statusessss']
+
+f = open("statuses.json", "w+")
+statuses = f.readlines()
+f.close()
+
+bulk = []
+for doc in tqdm(statuses):
+    bulk.append(json.loads(doc))
+    if len(bulk) > 500:
+        client.clientbulk_docs(bulk)
+        bulk = []
+
+if len(bulk):
+    client.bulk_docs(bulk)
+print("[*] recovered")
