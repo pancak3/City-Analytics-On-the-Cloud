5086ac78311c1f1a2298290ad3ccdf82907468b3
Qifan <plocircle@live.com>
2020-05-25 12:29:30 +0800

    analysis: simplise pearson py script

--- app/backend/analysis/corr.py
+++ app/backend/analysis/corr.py
@@ -1,32 +1,20 @@
 import sys
 import json
-import pandas as pd
-
-
-def to_df(json_dict):
-    keys, nums = [], []
-    for k, v in json_dict.items():
-        keys.append(k)
-        nums.append(v)
-    return pd.DataFrame(index=keys, columns=['values'], data=[nums]).sort_index()
-
-
-def get_pearson_corr(a, b):
-    pearson_corr = pd.DataFrame([a, b]).T.corr(method='pearson')
-    return pearson_corr.iloc[0, 1]
+import scipy.stats as stats
 
 
 def solve(a_in, b_in):
     """
-    output to stdout of two indexed nums, need to be with exactly the same ids
-    but the order of ids does not need to be the same
-    :param a_in: dict in json str, {"1":0.6, "68":0.3, ... ... , "2":45}
-    :param b_in: dict in json str, {"1":12, "2":8, ... ... , "68":0.5}
-    :return: print pearson correlation, 0.04
+    output to stdout of two num lists, indexed by the same location id
+    :param a_in: list in json str, [1, 3, 53, 1, 7]
+    :param b_in: list in json str, [4, 3, 2, 9, 6]
+    :return: print pearson correlation, (corr, p-value)
     """
-    a_json = json.loads(a_in)
-    b_json = json.loads(b_in)
-    print(get_pearson_corr(to_df(a_json)['values'], to_df(b_json)['values']))
+    a_list = json.loads(a_in)
+    b_list = json.loads(b_in)
+    res = stats.pearsonr(a_list, b_list)
+    print(res)
+    return res
 
 
 if __name__ == '__main__':
---
+++ app/frontend/src/scenarios/Sports.js
@@ -0,0 +1,346 @@
+import React, { useEffect, useState } from 'react';
+import Scenario from '../components/Scenario';
+import {
+    getCounts,
+    getKeyword,
+    getKeywordArea,
+    getHashTags,
+    getHashTagsByArea,
+} from '../helper/api';
+import PropTypes from 'prop-types';
+import {
+    ExpansionPanel,
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+    Input,
+    Typography,
+    Button,
+    Grid,
+    Box,
+    TableHead,
+    TableBody,
+    TableContainer,
+    TableCell,
+    TableRow,
+    Table,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { prepareGeoJSON } from './geo';
+
+function SportsExercise(props) {
+    const plainGeo = props.geojson;
+
+    const [counts, setCounts] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [keyword, setKeyword] = useState(false);
+    const [keywordData, setKeywordData] = useState(null);
+    const [keywordLoaded, setKeywordLoaded] = useState(true);
+    const [geojson, setGeoJSON] = useState(null);
+    const [freq, setFreq] = useState(null);
+    const [area, setArea] = useState(null);
+    const [areaCode, setAreaCode] = useState(null);
+    const [freqExpanded, setFreqExpanded] = useState(true);
+    const [keywordExpanded, setKeywordExpanded] = useState(true);
+    const [hashtagExpanded, setHashtagExpanded] = useState(true);
+    const [indExpanded, setIndExpanded] = useState(true);
+    const [marker, setMarker] = useState(null);
+    const [areaLoaded, setAreaLoaded] = useState(null);
+    const [areaHashtag, setAreaHashtag] = useState(null);
+    const [allHashtag, setAllHashtag] = useState(null);
+
+    // Load counts
+    useEffect(() => {
+        if (loaded) return;
+        setLoaded(true);
+        getCounts().then((data) => {
+            setCounts(data);
+        });
+
+        if (allHashtag) return;
+        getHashTags().then((data) => {
+            setAllHashtag(data);
+        });
+    }, [loaded, plainGeo, allHashtag]);
+
+    // Load keywords
+    useEffect(() => {
+        if (!keyword || keywordLoaded) return;
+        setKeywordLoaded(true);
+        getKeyword(keyword).then((data) => {
+            setKeywordData(data);
+        });
+    }, [keyword, keywordLoaded, plainGeo]);
+
+    // Which data should be used
+    const data = keywordData ? keywordData : counts;
+    // Which hashtag should be used
+    const hashtag = areaCode ? areaHashtag : allHashtag;
+
+    // When geojson is loaded
+    useEffect(() => {
+        if (plainGeo && data) {
+            setGeoJSON(prepareGeoJSON(plainGeo, data));
+        }
+    }, [plainGeo, data]);
+
+    // When data is changed, get top 5 frequency
+    useEffect(() => {
+        if (!data) return;
+        const freq = data
+            ? Object.keys(data).sort((a, b) => data[b] - data[a])
+            : null;
+        setFreq(freq.slice(0, 5));
+    }, [data]);
+
+    // Query for keyword
+    function submit_keyword() {
+        if (keyword === '' || !keyword) {
+            setKeywordLoaded(true);
+            setKeywordData(null);
+            setGeoJSON(prepareGeoJSON(plainGeo, counts));
+            setFreqExpanded(true);
+            setIndExpanded(false);
+        } else {
+            // load keyword data
+            setKeywordLoaded(false);
+            setFreqExpanded(true);
+            setIndExpanded(false);
+            setArea(null);
+            setAreaLoaded(false);
+        }
+    }
+
+    // Get tweets of area
+    useEffect(() => {
+        if (!areaCode || areaLoaded) return;
+
+        setAreaLoaded(true);
+        getKeywordArea(keyword, areaCode).then((data) => {
+            setArea(data);
+            setFreqExpanded(false);
+            setKeywordExpanded(false);
+            setIndExpanded(true);
+            setMarker(props.areaCentroid ? props.areaCentroid[areaCode] : null);
+        });
+        getHashTagsByArea(areaCode).then((data) => {
+            setAreaHashtag(data);
+        });
+    }, [areaLoaded, areaCode, keyword, props.areaCentroid]);
+
+    return (
+        <Scenario
+            data={geojson}
+            mode={'k'}
+            featureClick={(feature) => {
+                setAreaCode(feature.properties.feature_code);
+                setAreaLoaded(false);
+            }}
+            marker={marker}
+        >
+            <div>
+                <ExpansionPanel
+                    defaultExpanded
+                    expanded={keywordExpanded}
+                    onChange={(e) => setKeywordExpanded(!keywordExpanded)}
+                >
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <h5>Keyword Search</h5>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Grid>
+                            <Typography>
+                                Search by keyword (SA2 areas)
+                            </Typography>
+                            <Box mt={1}>
+                                <Grid className="d-flex">
+                                    <Input
+                                        margin="none"
+                                        placeholder="SportsExercise"
+                                        onChange={(e) => {
+                                            setKeyword(
+                                                e.target.value.toLowerCase()
+                                            );
+                                        }}
+                                    ></Input>
+                                    <Button
+                                        className="search"
+                                        variant="outlined"
+                                        color="primary"
+                                        onClick={() => submit_keyword()}
+                                    >
+                                        Search
+                                    </Button>
+                                </Grid>
+                            </Box>
+                        </Grid>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+            <div>
+                <ExpansionPanel
+                    defaultExpanded
+                    expanded={freqExpanded}
+                    onChange={(e) => setFreqExpanded(!freqExpanded)}
+                >
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <h5>Number of Tweets</h5>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Grid>
+                            {freq ? (
+                                freq.length > 0 ? (
+                                    <TableContainer>
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell component="th">
+                                                        SA2 Area
+                                                    </TableCell>
+                                                    <TableCell component="th">
+                                                        Count
+                                                    </TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {freq.map((area_code) => (
+                                                    <TableRow key={area_code}>
+                                                        <TableCell>
+                                                            {props.areaName
+                                                                ? props
+                                                                      .areaName[
+                                                                      area_code
+                                                                  ]
+                                                                : ''}
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            {data[area_code]}
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                ) : (
+                                    <p className="mt-4">No data found...</p>
+                                )
+                            ) : (
+                                <p className="mt-2">Loading...</p>
+                            )}
+                        </Grid>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+
+            {hashtag && (
+                <div>
+                    <ExpansionPanel
+                        defaultExpanded
+                        expanded={hashtagExpanded}
+                        onChange={(e) => setHashtagExpanded(!hashtagExpanded)}
+                    >
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <h5>Top hashtags</h5>
+                        </ExpansionPanelSummary>
+
+                        <ExpansionPanelDetails>
+                            {hashtag !== null ? (
+                                hashtag.length !== 0 ? (
+                                    <TableContainer className="mt-n2">
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell component="th">
+                                                        Hashtag
+                                                    </TableCell>
+                                                    <TableCell component="th">
+                                                        Count
+                                                    </TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {hashtag.map((h) => (
+                                                    <TableRow key={h[0]}>
+                                                        <TableCell>
+                                                            {h[0]}
+                                                        </TableCell>
+                                                        <TableCell>
+                                                            {h[1]}
+                                                        </TableCell>
+                                                    </TableRow>
+                                                ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                ) : (
+                                    <p className="mt-2">No hashtags found...</p>
+                                )
+                            ) : (
+                                <p className="mt-2">No data found...</p>
+                            )}
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </div>
+            )}
+
+            {area && (
+                <div id="indicative">
+                    <ExpansionPanel
+                        key="hashtag"
+                        defaultExpanded
+                        expanded={indExpanded}
+                        onChange={(e) => setIndExpanded(!indExpanded)}
+                    >
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <h5>Indicative tweets</h5>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails id="indicative">
+                            <Grid>
+                                <h6 className="mb-1">
+                                    {props.areaName[areaCode]} ({areaCode})
+                                </h6>
+                                <p id="count">
+                                    <strong>Number of tweets: </strong>
+                                    {data[areaCode] ? data[areaCode] : 0}
+                                </p>
+
+                                <Grid>
+                                    {area.map((tweet) => (
+                                        <div className="tweet" key={tweet.url}>
+                                            <p>{tweet.text}</p>
+                                            {tweet.url ? (
+                                                <React.Fragment>
+                                                    <FontAwesomeIcon
+                                                        icon={faTwitter}
+                                                    />
+                                                    <a
+                                                        href={tweet.url}
+                                                        rel="noopener noreferrer"
+                                                        target="_blank"
+                                                    >
+                                                        {tweet.url}
+                                                    </a>
+                                                </React.Fragment>
+                                            ) : (
+                                                <React.Fragment />
+                                            )}
+                                        </div>
+                                    ))}
+                                </Grid>
+                            </Grid>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </div>
+            )}
+        </Scenario>
+    );
+}
+
+SportsExercise.propTypes = {
+    geojson: PropTypes.array,
+    areaName: PropTypes.object,
+    areaCentroid: PropTypes.object,
+};
+
+export default SportsExercise;
